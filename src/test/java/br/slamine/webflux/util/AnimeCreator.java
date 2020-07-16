0b518a1b21b2abe3ca559bc03b51bacf572f250c
Simeão David Lamine <simeaolm@gmail.com>
2020-07-15 21:49:55 -0300

    Create Factory of Anime to be used in test

---
+++ src/test/java/br/slamine/webflux/util/AnimeCreator.java
@@ -0,0 +1,22 @@
+package br.slamine.webflux.util;
+
+import br.slamine.webflux.domain.Anime;
+
+public class AnimeCreator {
+
+    public static Anime createAnimeToBeSaved(){
+        return Anime.builder()
+                .name("South Park")
+                .build();
+    }
+    public static Anime createValidAnime(){
+        return Anime.builder()
+                .name("South Park")
+                .build();
+    }
+    public static Anime createValidUpdatedAnime(){
+        return Anime.builder()
+                .name("South Park. Season 2")
+                .build();
+    }
+}
