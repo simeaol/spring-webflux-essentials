bb7b5d608c69b94b9cfd89fd4276ce140d6bf8f6
Simeão David Lamine <simeaolm@gmail.com>
2020-07-15 22:44:15 -0300

    Fix factory anime with correct params for testing

--- src/test/java/br/slamine/webflux/util/AnimeCreator.java
+++ src/test/java/br/slamine/webflux/util/AnimeCreator.java
@@ -11,11 +11,13 @@ public class AnimeCreator {
     }
     public static Anime createValidAnime(){
         return Anime.builder()
+                .id(1)
                 .name("South Park")
                 .build();
     }
     public static Anime createValidUpdatedAnime(){
         return Anime.builder()
+                .id(1)
                 .name("South Park. Season 2")
                 .build();
     }
