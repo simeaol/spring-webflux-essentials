638135956166bd29c10ed1b0fc05afe4b82c1552
Simeão David Lamine <simeaolm@gmail.com>
2020-07-15 21:53:17 -0300

    Setup Service test - Import recommended dependency for JUnit in Spring framework <@ExtendWith(SpringExtensions.class> - InjectMocks in serivce - Mock repository - Setup blockHound with basic test provided in oficial github repo

---
+++ src/test/java/br/slamine/webflux/service/AnimeServiceTest.java
@@ -0,0 +1,45 @@
+package br.slamine.webflux.service;
+
+import br.slamine.webflux.domain.Anime;
+import br.slamine.webflux.repository.AnimeRepository;
+import br.slamine.webflux.util.AnimeCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(SpringExtension.class)
+public class AnimeServiceTest {
+
+    @InjectMocks
+    private AnimeService animeService;
+
+    @Mock
+    private AnimeRepository animeRepository;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetup(){
+        try{
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        }catch (Exception e){
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+    
+}
