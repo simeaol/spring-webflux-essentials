21f43af6c838247dbaca2b650cef5ace0985c6d2
Simeão David Lamine <simeaolm@gmail.com>
2020-07-16 22:59:05 -0300

    [Security] Generating encoded password using PasswordEnconderFactories just for testing purpose

--- src/main/java/br/slamine/webflux/SpringWebfluxEssentialsApplication.java
+++ src/main/java/br/slamine/webflux/SpringWebfluxEssentialsApplication.java
@@ -2,6 +2,7 @@ package br.slamine.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
@@ -13,6 +14,11 @@ public class SpringWebfluxEssentialsApplication {
 	}
 
 	public static void main(String[] args) {
+		System.out.println(
+				PasswordEncoderFactories
+						.createDelegatingPasswordEncoder()
+						.encode("slamine")
+		);
 		SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
 	}
 
